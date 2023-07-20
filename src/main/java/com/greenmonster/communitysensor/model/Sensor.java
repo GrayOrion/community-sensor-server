fbffa6a14649cfb9ab7450b142c61208b4002a2f
GrayOrion <grayorionable@gmail.com>
2023-07-20 09:37:52 -0700

    API testing cleaned
    * Moved content.http to sensor.http
    * Fixed all test runs
    * Removed problematic "@NotBlank" annotations in Validation - should look at how to implement  for enums

--- src/main/java/com/greenmonster/communitysensor/model/Sensor.java
+++ src/main/java/com/greenmonster/communitysensor/model/Sensor.java
@@ -2,24 +2,25 @@ package com.greenmonster.communitysensor.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 
 public record Sensor(
-        @NotBlank
         @Id
+        // TODO: how do I define not null here?!?, used to be @NotNull
         Integer id,
         @NotBlank
         String name,
         @NotBlank
         String desc,
         String locationDescription,
-        @NotBlank
+        @NotNull //was @NotBlank - TODO: how do I define not blank here?!?
         Type type,
-        @NotBlank
+        @NotNull //was @NotBlank - TODO: how do I define not blank here?!?
         Status status,
-        @NotBlank
+        @NotNull //was @NotBlank - TODO: how do I define not blank here?!?
         SensorClass sensorClass
 ) {
 }
