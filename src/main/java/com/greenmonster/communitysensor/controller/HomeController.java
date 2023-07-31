5156953caeb4d0fca1c69627b9745d3129baaf4e
GrayOrion <grayorionable@gmail.com>
2023-07-31 15:21:52 -0700

    Custom properties
    -Added custom property addition through @Value - just an example

---
+++ src/main/java/com/greenmonster/communitysensor/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.greenmonster.communitysensor.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class HomeController {
+    @Value("${cc.welcomeMessage: Default Welcome Message}")
+    private String welcomeMessage;
+
+    @Value("${cc.about}")
+    private String about;
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+
+        return Map.of(
+                "welcomeMessage", welcomeMessage,
+                "about", about
+        );
+    }
+}
